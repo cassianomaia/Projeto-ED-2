cbacec0c8e8e73a9f09d54fc56083b76f49c1a11
Mariana <cavichiolimariana@gmail.com>
2017-06-19 23:23:56 -0300

    Terrinhas posicionadas

--- Code/jogo.hpp
+++ Code/jogo.hpp
@@ -17,14 +17,39 @@ jogo::jogo(void){
 
 int jogo::Run(sf::RenderWindow &window){
 
-    /*sf::Texture vaquinha;
-    if(!vaquinha.loadFromFile("../Images/terra.bmp")){
+    //Imagem slot baixo direita
+    sf::Texture slot1;
+    if(!slot1.loadFromFile("../Images/vazio.png")){
         std::cout << "Error" << std::endl;
     }
-    sf::Sprite Vaquinha_acordada(vaquinha);
-    Vaquinha_acordada.setPosition(sf::Vector2f(150,150));*/
+    sf::Sprite Slot1(slot1);
+    Slot1.setPosition(sf::Vector2f(30,-10));
 
+    //Imagem slot baixo esquerda
+    sf::Texture slot2;
+    if(!slot2.loadFromFile("../Images/vazio.png")){
+        std::cout << "Error" << std::endl;
+    }
+    sf::Sprite Slot2(slot2);
+    Slot2.setPosition(sf::Vector2f(-36,-32));
 
+    //Imagem slot cima esquerda
+    sf::Texture slot3;
+    if(!slot3.loadFromFile("../Images/vazio.png")){
+        std::cout << "Error" << std::endl;
+    }
+    sf::Sprite Slot3(slot3);
+    Slot3.setPosition(sf::Vector2f(20,-70));
+
+    //Imagem slot cima direita
+    sf::Texture slot4;
+    if(!slot4.loadFromFile("../Images/vazio.png")){
+        std::cout << "Error" << std::endl;
+    }
+    sf::Sprite Slot4(slot4);
+    Slot4.setPosition(sf::Vector2f(86,-48));
+
+    //Background
     sf::Sprite background;
     sf::Texture texture;
     if(!texture.loadFromFile("../Images/background.bmp")){
@@ -79,7 +104,10 @@ int jogo::Run(sf::RenderWindow &window){
         window.clear();
         window.draw(background);
         player.draw(window);
-        //window.draw(Vaquinha_acordada);
+        window.draw(Slot1);
+        window.draw(Slot2);
+        window.draw(Slot3);
+        window.draw(Slot4);
         window.display();
 
     }
