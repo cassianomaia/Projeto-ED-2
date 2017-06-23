554c2e187bfa73bfc3e638e43acd3ea4458d9694
Unknown <cavichiolimariana@gmail.com>
2017-06-22 22:59:38 -0300

    How To Play

--- Code/howto.hpp
+++ Code/howto.hpp
@@ -5,6 +5,11 @@ class howto : public cScreen{
 private:
     sf::Texture texture;
     sf::Texture htp;
+    sf::Text text;
+    sf::Text button;
+    sf::Font font;
+    int posicaox, posicaoy, posicao;
+    int index;
 
 
 public:
@@ -18,35 +23,32 @@ howto::howto(void){
 
 int howto::Run(sf::RenderWindow &window){
 
-    if (!htp.loadFromFile("../Images/HTP.png")){
-        //erro
-    }
-    sf::Sprite howtoplay(htp);
-    howtoplay.setPosition(sf::Vector2f(100,200));
-
-
-    sf::Font font;
-    if (!font.loadFromFile("../Fonts/barn.otf")){
-        //erro
-    }
-
-    sf::Text text;
-    text.setFont(font);
-    text.setString("HOW TO PLAY");
-    text.setColor(sf::Color::Black);
-    text.setPosition(sf::Vector2f(175,60));
-    text.setCharacterSize(100);
+    index = 0;
+    posicao = 0;
 
     sf::Sprite background;
 
-    if(!texture.loadFromFile("../Images/fundo.bmp")){
+    if(!texture.loadFromFile("../Images/imagens_howtoplay/1.png")){
         std::cout << "Error" << std::endl;
     }
 
     background.setTexture(texture);
 
+    if (!font.loadFromFile("../Fonts/barn.otf")){
+        std::cout << "Error" << std::endl;
+    }
+
+    button.setFont(font);
+    button.setColor(sf::Color::Black);
+    button.setString("NEXT");
+    button.setPosition(sf::Vector2f(650,525));  
+    button.setCharacterSize(50);   
+
 
     while (window.isOpen()){
+    	posicaoy = sf::Mouse::getPosition(window).y;
+        posicaox = sf::Mouse::getPosition(window).x;
+
         sf::Event event;
         // Checa os eventos em loop
         while (window.pollEvent(event)){
@@ -58,11 +60,66 @@ int howto::Run(sf::RenderWindow &window){
                     return 0;
                 }
             }
+            switch(event.type){
+                case sf::Event::MouseButtonPressed:
+                    switch(event.key.code){
+                        case sf::Mouse::Left:
+                        	std::cout << posicaox << " , " << posicaoy << std::endl;
+                           if(posicaox >= 650 && posicaox <= 740){
+                            	if (posicaoy >= 545 && posicaoy <= 571){
+                            		posicao = 1;
+                            		index++;
+                            	}
+                            }
+                        break;
+                    }
+            	
+            	case sf::Event::MouseMoved:
+            		button.setColor(sf::Color::Black);
+                    if(posicaox >= 650 && posicaox <= 740){
+                        if (posicaoy >= 545 && posicaoy <= 571){
+                            button.setColor(sf::Color::Red);
+                        }
+                    }                       	
+                break;         
+            }
+
+        }
+
+        if (index == 1){
+        	if(!texture.loadFromFile("../Images/imagens_howtoplay/2.png")){
+    		}
+
+        }
+        else if (index == 2){
+        	if(!texture.loadFromFile("../Images/imagens_howtoplay/3.png")){
+		    }
+        }
+        else if (index == 3){
+        	if(!texture.loadFromFile("../Images/imagens_howtoplay/4.png")){
+        	}
+		    
+        }
+        else if (index == 4){
+        	if(!texture.loadFromFile("../Images/imagens_howtoplay/5.png")){
+		    }
+        }
+        else if (index == 5){
+        	if(!texture.loadFromFile("../Images/imagens_howtoplay/6.png")){
+		    }
+        }
+        else if (index == 6){
+        	if(!texture.loadFromFile("../Images/imagens_howtoplay/7.png")){
+		    }
+        }
+        else if(index == 7){
+        	return 0;
         }
 
         window.draw(background);
         window.draw(text);
-        window.draw(howtoplay);
+        //window.draw(howtoplay);
+        window.draw(button);
         window.display();
     }
 	return (-1);
