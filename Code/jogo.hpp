411297a14db1ca540d437f318c1957fc4ce5e3ef
cassianomaia <cassmala@gmail.com>
2017-06-27 14:51:27 -0300

    Few comments to get the thing

--- Code/jogo.hpp
+++ Code/jogo.hpp
@@ -328,8 +328,8 @@ int jogo::Run(sf::RenderWindow &window){
                             switch (mousestate = player.PositionPlants(posicaox, posicaoy)){
                                 case -1:
                                     if (Slot1.getGlobalBounds().contains(posicaox, posicaoy )){
-                                    	spots.Insere(mouseplanta, 1);
-                                        Slot1.setTexture(semeado);
+                                    	spots.Insere(mouseplanta, 1); //aqui temos de terirar a planta do mouse, e deixar uma planta generica com valores zero
+                                        Slot1.setTexture(semeado);    // /\ ou da pra usar o mouse state 
                                         countdown_slot1 = mouseplanta.getTempo(); 
                                         jataplantado1 = true;
 
@@ -708,7 +708,7 @@ int jogo::Run(sf::RenderWindow &window){
 		        break;
 
 		        }
-	            jataplantado1 = false;
+	            jataplantado1 = false; // qd crescer criar variavel cresceu, para qd clicarmos no slot, ele eliminar a planta
 	        }
         }
 
